adebbfe3ba5677d988aa43c61f5102d7b39a8120
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2023-12-26 11:44:32 -0500

    pushed solutions to exercise

--- exercise_code/count-zeroes.js
+++ exercise_code/count-zeroes.js
@@ -1,5 +1,21 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+  let firstZero = findFirst(arr);
+  if (firstZero === -1) return 0;
+
+  return arr.length - firstZero;
 }
 
-module.exports = countZeroes
+function findFirst(arr, low = 0, high = arr.length - 1) {
+  if (high >= low) {
+    let mid = low + Math.floor((high - low) / 2);
+    if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+      return mid;
+    } else if (arr[mid] === 1) {
+      return findFirst(arr, mid + 1, high);
+    }
+    return firstFirst(arr, low, mid - 1);
+  }
+  return -1;
+}
+
+module.exports = countZeroes;
